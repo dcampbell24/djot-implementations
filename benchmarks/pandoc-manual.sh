1dd9f9412dc10a5bd9554e95839866e2d5caf762
David Campbell <dcampbell24@gmail.com>
2024-12-03 13:00:32 -0500

    Modify pandoc-manual.sh

--- benchmarks/pandoc-manual.sh
+++ benchmarks/pandoc-manual.sh
@@ -5,7 +5,7 @@ PANDOC_MANUAL_DJ=pandoc-manual.dj
 
 wget --no-clobber --output-document=$PANDOC_MANUAL_DJ $PANDOC_MANUAL
 
-hyperfine --warmup 20 --shell=none --export-markdown implementations.md --sort mean-time \
+hyperfine --warmup 20 --shell=none --sort mean-time \
 --command-name Go "$HOME/go/bin/godjot $PANDOC_MANUAL_DJ" \
 --command-name Haskell "$HOME/.cabal/bin/djoths $PANDOC_MANUAL_DJ" \
 --command-name JavaScript "djot $PANDOC_MANUAL_DJ" \
