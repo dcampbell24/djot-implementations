3b61e9c4e82582e8ea585b46f8e1b8c3d3ff1c8e
David Campbell <dcampbell24@gmail.com>
2024-12-03 18:23:55 -0500

    Actually run the Go version on something.

--- benchmarks.sh
+++ benchmarks.sh
@@ -6,7 +6,7 @@ PANDOC_MANUAL_DJ=pandoc-manual.dj
 wget --no-clobber --output-document=$PANDOC_MANUAL_DJ $PANDOC_MANUAL
 
 hyperfine --warmup 20 --shell=none --sort mean-time \
---command-name Go "$HOME/go/bin/godjot $PANDOC_MANUAL_DJ" \
+--command-name Go "$HOME/go/bin/godjot -from $PANDOC_MANUAL_DJ" \
 --command-name Haskell "$HOME/.cabal/bin/djoths $PANDOC_MANUAL_DJ" \
 --command-name JavaScript "djot $PANDOC_MANUAL_DJ" \
 --command-name Lua "$HOME/.luarocks/bin/djot $PANDOC_MANUAL_DJ" \
@@ -20,7 +20,7 @@ wget --no-clobber --output-document=$TARTAN_WIKIPEDIA_HTML $TARTAN_WIKIPEDIA
 pandoc --from=html --to=djot --output=$TARTAN_WIKIPEDIA_DJ $TARTAN_WIKIPEDIA_HTML
 
 hyperfine --warmup 20 --shell=none --sort mean-time \
---command-name Go "$HOME/go/bin/godjot $TARTAN_WIKIPEDIA_DJ" \
+--command-name Go "$HOME/go/bin/godjot -from $TARTAN_WIKIPEDIA_DJ" \
 --command-name Haskell "$HOME/.cabal/bin/djoths $TARTAN_WIKIPEDIA_DJ" \
 --command-name JavaScript "djot $TARTAN_WIKIPEDIA_DJ" \
 --command-name Lua "$HOME/.luarocks/bin/djot $TARTAN_WIKIPEDIA_DJ" \
