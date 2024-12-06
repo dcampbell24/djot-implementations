377669237f16759c7a0179ad526fe449f847df0c
David Campbell <dcampbell24@gmail.com>
2024-12-06 16:52:43 -0500

    Run run-benchmarks from djot-implementations.

---
+++ install-djots-debian.sh
@@ -0,0 +1,22 @@
+#!/bin/dash -e
+
+# Go
+go install github.com/sivukhin/godjot@latest # Installed in: ~/go/bin/godjot
+
+# Haskell
+cabal update
+cabal install djot # Installed in: ~/.cabal/bin/djoths
+
+# JavaScript
+npm install -g @djot/djot # Installed in: ~/.nvm/versions/node/v22.11.0/bin/djot
+
+# Lua
+luarocks install --local djot # Installed in: ~/.luarocks/bin/djot
+
+# Rust
+rustup update stable
+rustup default stable
+cargo install jotdown # Installed in: ~/.cargo/bin/jotdown
+
+# Hyperfine
+cargo install hyperfine # Installed in: ~/.cargo/bin/hyperfine
