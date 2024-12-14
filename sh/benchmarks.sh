794021f46d742b8ca810a0209c3fbf0424487dba
David Campbell <dcampbell24@gmail.com>
2024-12-13 22:13:34 -0500

    Add the pandoc files to the project.

--- sh/benchmarks.sh
+++ sh/benchmarks.sh
@@ -2,10 +2,7 @@
 
 mkdir tmp || true
 
-PANDOC_MANUAL=https://github.com/jgm/djot/files/11900633/pandoc-manual.txt
-PANDOC_MANUAL_DJ=tmp/pandoc-manual.dj
-
-wget --quiet --no-clobber --output-document=$PANDOC_MANUAL_DJ $PANDOC_MANUAL || true
+PANDOC_MANUAL_DJ=benchmark-files/pandoc-manual.dj
 
 [ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh"
 DJOT_JS=$(echo $(nvm which current) | sed -e 's/node$/djot/')
@@ -19,12 +16,7 @@ hyperfine --warmup 20 --shell=none --export-markdown tmp/pandoc-manual-benchmark
 --command-name Lua "$HOME/.luarocks/bin/djot $PANDOC_MANUAL_DJ" \
 --command-name Rust "jotdown $PANDOC_MANUAL_DJ"
 
-TARTAN_WIKIPEDIA=https://en.wikipedia.org/wiki/Tartan
-TARTAN_WIKIPEDIA_HTML=tmp/tartan-wikipedia.html
-TARTAN_WIKIPEDIA_DJ=tmp/tartan-wikipedia.dj
-
-wget --quiet --no-clobber --output-document=$TARTAN_WIKIPEDIA_HTML $TARTAN_WIKIPEDIA || true
-pandoc --from=html --to=djot --output=$TARTAN_WIKIPEDIA_DJ $TARTAN_WIKIPEDIA_HTML
+TARTAN_WIKIPEDIA_DJ=benchmark-files/tartan-wikipedia.dj
 
 hyperfine --warmup 20 --shell=none --export-markdown tmp/tartan-wikipedia-benchmarks.md \
 --sort mean-time \
@@ -38,11 +30,11 @@ hyperfine --warmup 20 --shell=none --export-markdown tmp/tartan-wikipedia-benchm
 cat > tmp/benchmarks.md <<EOF
 ### Running on $(lscpu | grep "Model name: [-|(|)| |a-z|A-Z|0-9]*" | sed -e 's/Model name: *//') at $(date --rfc-3339=seconds)  
 
-Time to render pandoc-manual.dj ($(echo $(du -h tmp/pandoc-manual.dj) | sed -r 's/([0-9.MK]+)[ .a-zA-Z/-]+/\1/')) into html:
+Time to render pandoc-manual.dj ($(echo $(du -h benchmark-files/pandoc-manual.dj) | sed -r 's/([0-9.MK]+)[ .a-zA-Z/-]+/\1/')) into html:
 
 $(cat tmp/pandoc-manual-benchmarks.md)
 
-Time to render tartan-wikipedia.dj ($(echo $(du -h tmp/tartan-wikipedia.dj) | sed -r 's/([0-9.MK]+)[ .a-zA-Z/-]+/\1/')) into html:
+Time to render tartan-wikipedia.dj ($(echo $(du -h benchmark-files/tartan-wikipedia.dj) | sed -r 's/([0-9.MK]+)[ .a-zA-Z/-]+/\1/')) into html:
 
 $(cat tmp/tartan-wikipedia-benchmarks.md)
 
